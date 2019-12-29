5e8733a5b1a60ea782372fca155d22731bece277
Fil923 <filippo.nardi92@gmail.com>
2019-12-29 14:23:28 +0100

    Interfaces

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -1,17 +1,38 @@
 // an interface describes how an object should look like... it's like a custome type
 // an interface it's like a contract for a class
-interface Greetable {
-  name: string;
 
+// this can be done with an interface
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+
+let add: AddFn;
+
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
+
+// You can implement inheritance even in interfaces
+interface Named {
+  // in interfaces you can set readonly property
+  // if a property is readonly, you can't change after is setted once
+  readonly name?: string;
+  outputName?: string;
+}
+
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 //class can implement more than only one interface!!
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 27;
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
   }
 
   greet(phrase: string) {
@@ -21,7 +42,9 @@ class Person implements Greetable {
 
 let user1: Greetable;
 
-user1 = new Person("Filippo");
+user1 = new Person();
+// this is not possible because name is readonly
+//user1.name = "Max";
 
 user1.greet("Hello there - I am");
 console.log(user1);
