5f8021414675a7aa7104a993f3f3190ffedc1df9
nardif <filippo.nardi.external@ynap.com>
2020-01-27 14:15:37 +0100

    generic classes

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -52,3 +52,42 @@ function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) 
 }
 
 extractAndConvert({ name: "Max" }, "name");
+
+// generic class
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Filippo");
+textStorage.addItem("Marika");
+textStorage.removeItem("Filippo");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// we can use multiple types of data
+// const numberStorage = new DataStorage<number | string>();
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({ name: "Filippo" });
+// objStorage.addItem({ name: "Marika" });
+// // ...
+// objStorage.removeItem({ name: "Filippo" });
+// console.log(objStorage.getItems());
