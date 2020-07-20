c20c7679c355f3efa08b1ed2bd92f614f9d3b8a7
nardif <filippo.nardi.external@ynap.com>
2020-07-20 23:25:16 +0200

    third party libraries setup

---
+++ libs-02-lodash-and-types/src/app.ts
@@ -0,0 +1,10 @@
+import _ from "lodash";
+
+/**
+ * declare is used to tell Typescript: don't worry, it is declared and it is a string
+ */
+// declare var GLOBAL: string;
+
+console.log(_.shuffle([1, 2, 3]));
+
+// console.log(GLOBAL);
