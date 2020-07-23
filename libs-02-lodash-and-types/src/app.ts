fd49d5df636045f2e61d7daa35d6cf57a923eacf
nardif <filippo.nardi.external@ynap.com>
2020-07-23 12:36:14 +0200

    Class validator library

--- libs-02-lodash-and-types/src/app.ts
+++ libs-02-lodash-and-types/src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import { plainToClass } from "class-transformer";
+import { validate } from "class-validator";
 
 import { Product } from "./product.model";
 
@@ -7,6 +8,16 @@ const products = [
   { title: "A gift", price: 12.99 },
   { title: "A Book", price: 12.99 },
 ];
+
+const newProd = new Product("", -5.29);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("Validation Errors");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
 
 // const p1 = new Product("A Book", 12.99);
 
