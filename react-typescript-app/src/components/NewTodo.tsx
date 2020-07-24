d30c27c717f9c7cb0b3fbe8024d65a459964fc1a
nardif <filippo.nardi.external@ynap.com>
2020-07-24 17:54:20 +0200

    React Todo app with Typescript

---
+++ react-typescript-app/src/components/NewTodo.tsx
@@ -0,0 +1,29 @@
+import React, { useRef } from "react";
+
+import "./NewTodo.css";
+
+interface NewTodoProps {
+  onAddTodo: (todoText: string) => void;
+}
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+
+  const todoSubmitHanlder = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={todoSubmitHanlder}>
+      <div className="form-control">
+        <label htmlFor="todo-text">Todo Text</label>
+        <input type="text" id="todo-text" ref={textInputRef}></input>
+      </div>
+      <button type="submit">ADD TODO</button>
+    </form>
+  );
+};
+
+export default NewTodo;
